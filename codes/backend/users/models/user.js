99c5b81aab5179f9beaa3d6375752caaafaf5818
Tamir Banay <banay9329@gmail.com>
2024-07-25 14:19:39 +0300

    cahnge in the user model the phonNumber  to unique

--- codes/backend/users/models/user.js
+++ codes/backend/users/models/user.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, unique: true, required: true },
   username: String,
   password: String,
-  phonNumber: String,
+  phonNumber: { type: String, unique: true, required: true },
   userStatus: {
     type: String,
     enum: ["Connected", "disConnected"],
