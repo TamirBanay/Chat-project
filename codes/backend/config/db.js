af8948de318e3ef0f81f9120cf39d58fb6e562d4
Tamir Banay <banay9329@gmail.com>
2024-07-27 15:40:36 +0300

    Updated MongoDB connection URIl

--- codes/backend/config/db.js
+++ codes/backend/config/db.js
@@ -1,9 +1,16 @@
-mongoose
-  .connect(process.env.MONGODB_URI, {
-  })
-  .then(() => {
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connected");
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-  });
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
