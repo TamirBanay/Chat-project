d66ef63e3df8eaa9859a9fc90f4c2b10770644bf
Tamir Banay <banay9329@gmail.com>
2024-07-27 15:22:51 +0300

    l

--- codes/backend/config/db.js
+++ codes/backend/config/db.js
@@ -1,16 +1,9 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+mongoose
+  .connect(process.env.MONGODB_URI, {
+  })
+  .then(() => {
     console.log("MongoDB connected");
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-};
-
-module.exports = connectDB;
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
