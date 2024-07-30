cd3cccd7c328b1b63dee8c080dab5ea75eb04af6
Tamir Banay <banay9329@gmail.com>
2024-07-30 13:57:49 +0300

    new camra

--- codes/frontend/chat-app/src/components/camera/Camera.js
+++ codes/frontend/chat-app/src/components/camera/Camera.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useState } from "react";
 import io from "socket.io-client";
 import { useNavigate, useParams } from "react-router-dom";
 
@@ -7,101 +7,45 @@ const socket = io(
 );
 
 function Camera() {
-  const videoRef = useRef(null);
-  const canvasRef = useRef(null);
   const { chatId, userId } = useParams();
   const navigate = useNavigate();
   const [messages, setMessages] = useState([]);
 
-  useEffect(() => {
-    startCamera();
+  const handlePictureChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const image = reader.result;
 
-    return () => {
-      if (videoRef.current && videoRef.current.srcObject) {
-        videoRef.current.srcObject.getTracks().forEach((track) => track.stop());
-      }
-    };
-  }, []);
+        // שליחת התמונה בסוקט
+        socket.emit("sendMessage", { chatId, message: image, userId });
 
-  const startCamera = () => {
-    navigator.getUserMedia =
-      navigator.getUserMedia ||
-      navigator.webkitGetUserMedia ||
-      navigator.mozGetUserMedia;
+        // עדכון ההודעות כדי להוסיף את ההודעה עם התמונה
+        setMessages((prevMessages) => [
+          ...prevMessages,
+          { userId, message: image },
+        ]);
 
-    if (navigator.getUserMedia) {
-      navigator.getUserMedia(
-        { audio: true, video: { width: 1280, height: 720 } },
-        function (stream) {
-          var video = document.querySelector("video");
-          video.srcObject = stream;
-          video.onloadedmetadata = function (e) {
-            video.play();
-          };
-        },
-        function (err) {
-          console.log("The following error occurred: " + err.name);
-        }
-      );
-    } else {
-      console.log("getUserMedia not supported");
+        navigate(`/chats/${userId}/${chatId}`);
+      };
+      reader.readAsDataURL(file);
     }
-  };
-
-  const takePicture = () => {
-    const context = canvasRef.current.getContext("2d");
-    context.drawImage(
-      videoRef.current,
-      0,
-      0,
-      canvasRef.current.width,
-      canvasRef.current.height
-    );
-    const image = canvasRef.current.toDataURL("image/png");
-
-    socket.emit("sendMessage", { chatId, message: image, userId });
-
-    setMessages((prevMessages) => [
-      ...prevMessages,
-      { userId, message: image },
-    ]);
-
-    navigate(`/chats/${userId}/${chatId}`);
   };
 
   return (
     <div className="camera-main">
-      {/* <div>
-        <video ref={videoRef} autoPlay className="camera-video"></video>
-        <canvas
-          ref={canvasRef}
-          width="640"
-          height="480"
-          style={{ display: "none" }}
-        ></canvas>
-      </div> */}
-      <label for="selfie">Take a picture of your face:</label>
-
-      <input
-        type="file"
-        id="selfie"
-        name="selfie"
-        accept="image/*"
-        capture="user"
-      />
-
-      <label for="picture">Take a picture using back facing camera:</label>
-
+      <label htmlFor="picture">Take a picture using back facing camera:</label>
       <input
         type="file"
         id="picture"
         name="picture"
         accept="image/*"
         capture="environment"
+        onChange={handlePictureChange}
       />
-
-      {/* <button onClick={takePicture}>Take Picture</button> */}
     </div>
   );
 }
+
 export default Camera;
