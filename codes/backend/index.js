81fdf81bd24a8e2476da7f1015b957da655c04e4
Tamir Banay <banay9329@gmail.com>
2024-08-06 14:39:48 +0300

    change the port

--- codes/backend/index.js
+++ codes/backend/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const server = require("./app");
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4001;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
